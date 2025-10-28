0a34ea78a975da026c2879609eb99d1795508a49
Henrique O. <lemonheadch@outlook.com>
2025-10-28 15:06:09 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -203,8 +203,8 @@ export default function Home() {
             <h3 className="text-2xl font-semibold">
               Sumito Media | Meme Scholar - Vending Machines Aficionado - Father - Husband
             </h3>
-            <div><h6 style="font-size: 10px" className="text-2xl font-semibold">
-              Website by <a id="heart" href="https://www.youtube.com/@ricksahuman" target="_blank">Rick's a Human</a>
+            <div><h6 className="text-2xl font-semibold">
+              Website by <a style="font-size: 10px" id="heart" href="https://www.youtube.com/@ricksahuman" target="_blank">Rick's a Human</a>
             </h6></div>
           </div>
           <div id="socialicons" className="social-icons-fixed">
@@ -226,6 +226,7 @@ export default function Home() {
     </main>
   );
 }
+
 
 
 
