f1af3c89da08e61e2107c5a474d2c70cf4ece342
Henrique O. <lemonheadch@outlook.com>
2025-10-28 17:12:13 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -219,7 +219,7 @@ export default function Home() {
             <a href="https://www.instagram.com/sumitomedia/" id="instagram" target="blank" className="text-3xl heartbeat-6"><i className="fab fa-instagram"></i></a>
             <a href="https://sumitomedia.com/" target="blank" id="heart" className="text-2xl mt-2 heartbeat-2"><i className="fa fa-heart"></i></a>
           </div>
-          <h6 id="plug" className="mobile-text plugin">
+          <h6 id="plug" className="new-mob mobile-text plugin">
               Website by <a id="heart" href="https://www.youtube.com/@ricksahuman" target="_blank">Rick's a Human</a>
             </h6>
         </div>
@@ -232,4 +232,5 @@ export default function Home() {
     </main>
   );
 }
+
 
