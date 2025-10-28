2f7d0fda9c0ff1ecfac9b116ec2ccbf7cc00ea9c
Henrique O. <lemonheadch@outlook.com>
2025-10-28 13:41:10 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwind()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
